bcecb6dae59cf990d4d8cbc9a195fe0ff6d0d99f
Pedro <pedro@pepicrft.me>
2024-04-27 16:33:11 +0200

    Try to obtain the temporary directory using NSTemporaryDirectory()

--- Sources/FileSystem/FileSystem.swift
+++ Sources/FileSystem/FileSystem.swift
@@ -1,4 +1,14 @@
 import Foundation
-// import Logging
-// import Path
-// import TSCBasic
+import Logging
+import Path
+
+public protocol FileSysteming {
+    func createTemporaryDirectory() async throws -> AbsolutePath
+}
+
+public struct FileSystem: FileSysteming {
+    public func createTemporaryDirectory() async throws -> AbsolutePath {
+        let temporaryDirectory = NSTemporaryDirectory()
+        return try AbsolutePath(validating: temporaryDirectory)
+    }
+}
--- Tests/FileSystemTests/FileSystemTests.swift
+++ Tests/FileSystemTests/FileSystemTests.swift
@@ -1,1 +1,23 @@
 import XCTest
+@testable import FileSystem
+
+final class FileSystemTests: XCTestCase {
+    var subject: FileSystem!
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        subject = FileSystem()
+    }
+    
+    override func tearDown() async throws {
+        subject = nil
+        try await super.tearDown()
+    }
+    
+    func test_createTemporaryDirectory_returnsAValidDirectory() async throws {
+        // Given
+        let got = try await subject.createTemporaryDirectory()
+        
+        print(got)
+    }
+}
