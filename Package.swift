6c6b87028f80cce8aa322fd30b5b77f25eb4c210
fortmarek <marekfort@me.com>
2024-10-24 11:22:33 +0200

    Add note about a fork

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.74.0")),
         .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.6.1")),
         .package(url: "https://github.com/weichsel/ZIPFoundation", .upToNextMajor(from: "0.9.19")),
+        // We're depending on a fork until the following PR is merged: https://github.com/davbeck/swift-glob/pull/14
         .package(url: "https://github.com/tuist/swift-glob", .upToNextMajor(from: "0.2.2")),
     ],
     targets: [
